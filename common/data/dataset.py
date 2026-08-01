05ea8a5068ece8a276e91db7450737426e77dc50
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-01 22:52:20 +0330

    Update dataset.py

--- common/data/dataset.py
+++ common/data/dataset.py
@@ -1,45 +1,63 @@
 from __future__ import annotations
 
+from pathlib import Path
+
+import numpy as np
+import torch
+
 from torch import Tensor
 from torch.utils.data import Dataset
-import torch
 
 
 class TextDataset(Dataset):
-    """
-    Dataset for next-token prediction.
-    """
 
     def __init__(
-        self,
-        token_ids: list[int],
-        block_size: int,
-    ) -> None:
-        if block_size <= 0:
-            raise ValueError("block_size must be greater than zero.")
+            self,
+            path: Path,
+            block_size: int,
+    ):
 
-        if len(token_ids) <= block_size:
-            raise ValueError(
-                "token_ids must contain more elements than block_size."
-            )
+        self._tokens = np.memmap(
+            path,
+            dtype=np.uint16,
+            mode="r",
+        )
 
-        self._token_ids = token_ids
         self._block_size = block_size
 
-    def __len__(self) -> int:
-        return len(self._token_ids) - self._block_size
+
+        if len(self._tokens) <= block_size:
+            raise ValueError(
+                "Dataset is smaller than block size"
+            )
+
+
+    def __len__(self):
+
+        return len(self._tokens) - self._block_size
+
 
     def __getitem__(
-        self,
-        index: int,
+            self,
+            index: int,
     ) -> tuple[Tensor, Tensor]:
-        start = index
-        end = start + self._block_size
 
-        input_ids = self._token_ids[start:end]
-        target_ids = self._token_ids[start + 1:end + 1]
+        x = self._tokens[
+            index:index + self._block_size
+        ]
+
+        y = self._tokens[
+            index + 1:index + self._block_size + 1
+        ]
+
 
         return (
-            torch.tensor(input_ids, dtype=torch.long),
-            torch.tensor(target_ids, dtype=torch.long),
+            torch.tensor(
+                x,
+                dtype=torch.long,
+            ),
+            torch.tensor(
+                y,
+                dtype=torch.long,
+            ),
         )
