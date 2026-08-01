c277ba0649dab34a0172d1b839fe0e26b19323f6
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-01 23:32:38 +0330

    Create test_gpt.py

---
+++ test_gpt.py
@@ -0,0 +1,35 @@
+import torch
+
+from common.configs.model_config import ModelConfig
+from mini_gpt.model import MiniGPT
+from common.data.vocabulary import Vocabulary
+
+
+vocabulary = Vocabulary.load(
+    ModelConfig.VOCAB_PATH
+)
+print(
+    "Vocabulary size:",
+    vocabulary.size
+)
+
+model = MiniGPT(
+    vocab_size=500,
+    block_size=128,
+    embedding_dim=128,
+    num_heads=4,
+    num_layers=4,
+)
+
+
+x = torch.randint(
+    0,
+    500,
+    (32,128)
+)
+
+
+logits = model(x)
+
+
+print(logits.shape)
