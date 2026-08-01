bb54f8d14106dbf01c5d07bd231ad5f5f556528b
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-01 23:32:21 +0330

    Create config.py

---
+++ mini_gpt/config.py
@@ -0,0 +1,18 @@
+class GPTConfig:
+
+    BLOCK_SIZE = 128
+
+    EMBEDDING_DIM = 128
+
+    NUM_HEADS = 4
+
+    NUM_LAYERS = 4
+
+    DROPOUT = 0.1
+
+
+    BATCH_SIZE = 32
+
+    LEARNING_RATE = 3e-4
+
+    EPOCHS = 10
