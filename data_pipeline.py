396c5194b72ec0021f27446288d2cbc15b2f6115
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-01 22:49:43 +0330

    Update data_pipeline.py

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,40 +1,114 @@
 from pathlib import Path
 
-from torch.utils.data import DataLoader
-
-from common.configs.data_config import DataConfig
 from common.data.builders.character_vocabulary_builder import (
     CharacterVocabularyBuilder,
 )
+
 from common.data.character_tokenizer import CharacterTokenizer
 from common.data.readers.csv_reader import CSVReader
-from common.data.dataset import TextDataset
+from common.data.preprocessing.dataset_preparer import DatasetPreparer
 
-reader = CSVReader(
-    Path("common/data/raw/train.csv")
+
+RAW_DATA_DIR = Path(
+    "common/data/raw"
 )
 
-texts = reader.read()
+PROCESSED_DATA_DIR = Path(
+    "common/data/processed"
+)
 
-builder = CharacterVocabularyBuilder()
 
-vocabulary = builder.build(
-    "\n".join(texts)
-)
-tokenizer = CharacterTokenizer(vocabulary)
-tokens = tokenizer.encode(
-    "\n".join(texts)
-)
-dataset = TextDataset(
-    token_ids=tokens,
-    block_size=DataConfig.BLOCK_SIZE,
-)
-loader = DataLoader(
-    dataset,
-    batch_size=DataConfig.TRAIN_BATCH_SIZE,
-    shuffle=True,
-)
-x, y = next(iter(loader))
+TRAIN_FILE = RAW_DATA_DIR / "train.csv"
+VALIDATION_FILE = RAW_DATA_DIR / "validation.csv"
 
-print(x.shape)
-print(y.shape)
+
+VOCAB_FILE = PROCESSED_DATA_DIR / "vocab.json"
+TRAIN_BIN = PROCESSED_DATA_DIR / "train.bin"
+VALIDATION_BIN = PROCESSED_DATA_DIR / "validation.bin"
+
+
+def main():
+
+    print("Creating dataset preparer...")
+
+    preparer = DatasetPreparer(
+        CharacterVocabularyBuilder()
+    )
+
+
+    print("Building vocabulary...")
+
+
+    train_reader = CSVReader(
+        TRAIN_FILE
+    )
+
+
+    vocabulary = preparer.prepare_vocabulary(
+        train_reader.read(),
+        VOCAB_FILE,
+    )
+
+
+    print(
+        "Vocabulary size:",
+        len(vocabulary)
+    )
+
+
+    tokenizer = CharacterTokenizer(
+        vocabulary
+    )
+
+
+    print("Encoding train dataset...")
+
+
+    train_reader = CSVReader(
+        TRAIN_FILE
+    )
+
+
+    preparer.encode_to_file(
+        train_reader.read(),
+        tokenizer,
+        TRAIN_BIN,
+    )
+
+
+    print("Encoding validation dataset...")
+
+
+    validation_reader = CSVReader(
+        VALIDATION_FILE
+    )
+
+
+    preparer.encode_to_file(
+        validation_reader.read(),
+        tokenizer,
+        VALIDATION_BIN,
+    )
+
+
+    print("Dataset preparation completed.")
+
+    print()
+
+    print("Generated files:")
+
+    print(
+        VOCAB_FILE
+    )
+
+    print(
+        TRAIN_BIN
+    )
+
+    print(
+        VALIDATION_BIN
+    )
+
+
+if __name__ == "__main__":
+    main()
